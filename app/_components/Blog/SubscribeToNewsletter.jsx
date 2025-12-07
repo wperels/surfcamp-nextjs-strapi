d1a586224539132fd33eda3ae2d2afbae0141300
WPerelstein <wperelstein@gmail.com>
2025-12-07 07:02:09 -0700

    Finshed newsletter and featured items sections

---
+++ app/_components/Blog/SubscribeToNewsletter.jsx
@@ -0,0 +1,49 @@
+"use client"
+import { useState } from "react"
+
+const SubscribeToNewsletter = () => {
+
+const [email, setEmail] = useState("");
+const [hasSignedUp, setHasSignedUp] = useState(false);  
+const onChange = (e) => {
+  setEmail(e.target.value)
+}
+
+  const onSubmit = async (e) => {
+      e.preventDefault()
+
+  console.log("pressed")
+  // Send email to Strapi
+      if (email.length) {
+        setHasSignedUp(true)
+      }
+
+  }
+  return (
+      
+    <section className="newsletter">
+      { hasSignedUp ? (
+          <h4 className="newsletter__thanks">Thank you for subscribing to our newsletter!</h4>
+      ) : (
+        <>
+          <div className="newsletter__info">
+          <h4>Subscribe to our newsletter</h4>
+          <p className="copy">"Unlock Exclusive Insights and Stay In the Know
+            – Subscribe to Our Newsletter Today to always stay in touch"</p>
+          </div>
+            <form className="newsletter__form" onSubmit={onSubmit}>
+              <input type="text" 
+              className="newsletter__email input" 
+              placeholder="Enter your email address" 
+              value={email}
+              onChange={onChange}
+              />
+              <button type="submit" className="newsletter__subscribe btn btn--medium btn--turquoise">SUBSCRIBE</button>
+            </form>
+        </>
+      )}
+    </section>
+  )
+}
+
+export default SubscribeToNewsletter
---
+++ app/_components/FeaturedItems/FeaturedItems.jsx
@@ -0,0 +1,20 @@
+import FeaturedArticle from "./FeaturedArticle";
+
+const FeaturedItems = ( {headline, items} ) => {
+  return (
+    <section className="fetured-items">
+    <h3 className="featured-items__headline">
+      {headline || "Our featured articles"}
+    </h3>
+    <div className="featured-items__container">
+      {items.map((item) => (
+        <FeaturedArticle key={item.slug} article={item} />
+      ))}
+    </div>
+    <button className="btn btn--medium btn--turquoise">Read more</button>
+    </section>
+  )
+}
+
+
+export default FeaturedItems;
