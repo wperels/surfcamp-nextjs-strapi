1b885b6da721232ac12bc1a7dba626faa9f836c4
WPerelstein <wperelstein@gmail.com>
2025-12-16 07:11:43 -0700

    fixed HighlightArticle and FeaturedArticle again

--- app/_components/FeaturedItems/FeaturedItems.jsx
+++ app/_components/FeaturedItems/FeaturedItems.jsx
@@ -1,6 +1,12 @@
+"use client"
+
 import FeaturedArticle from "./FeaturedArticle";
+import { useState } from "react";
 
 const FeaturedItems = ( {headline, items} ) => {
+  const [itemNumber, setItemNumber] = useState(3)
+
+  const onShowMore = () => {}
   return (
     <section className="fetured-items">
     <h3 className="featured-items__headline">
@@ -11,7 +17,7 @@ const FeaturedItems = ( {headline, items} ) => {
         <FeaturedArticle key={item.slug} article={item} />
       ))}
     </div>
-    <button className="btn btn--medium btn--turquoise">Read more</button>
+    <button className="btn btn--medium btn--turquoise" onClick={onShowMore}>See more</button>
     </section>
   )
 }
