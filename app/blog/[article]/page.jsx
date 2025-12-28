90b2d333d64b6819e4779d0893308ab710f63564
WPerelstein <wperelstein@gmail.com>
2025-12-28 08:31:08 -0700

    ArticleComponent and ArticleOverview completed

--- app/blog/[article]/page.jsx
+++ app/blog/[article]/page.jsx
@@ -1,5 +1,7 @@
 import ArticleIntro from "@/app/_components/Blog/ArticleIntro";
 import {  fetchBlogArticles, fetchDataFromStrapi } from "../../../utils/strapi.utils"; 
+import ArticleOverview from "@/app/_components/Blog/ArticleOverview";
+import ArticleComponent from "@/app/_components/Blog/ArticleComponent";
 
 
 export default async function Page({ params }) {
@@ -17,6 +19,12 @@ export default async function Page({ params }) {
   return (
     <main>
       <ArticleIntro article={article} />
+     <section className="article-section">   
+      <ArticleOverview article={article} />
+      {article.articleContent.map((component) => (
+        <ArticleComponent key={component.id} component={component} />
+      ) )}
+     </section>
     </main>
   );
 }
