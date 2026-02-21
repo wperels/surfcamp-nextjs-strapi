ad04bbf92d00731d14611dab856419d8445409d1
WPerelstein <wperelstein@gmail.com>
2026-02-21 16:58:12 -0700

    Add hamburger menu + fix responsive header all breakpoints again

--- app/_components/Layout/Header.jsx
+++ app/_components/Layout/Header.jsx
@@ -1,5 +1,5 @@
 "use client"
-import Image from 'next/image';
+//import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { useState } from 'react';
