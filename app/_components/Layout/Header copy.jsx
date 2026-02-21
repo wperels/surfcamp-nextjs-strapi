0dc15a28005db8e6c3c9b3e9e00d97a06ec21da9
WPerelstein <wperelstein@gmail.com>
2026-02-21 16:58:12 -0700

    Add hamburger menu + fix responsive header all breakpoints again

---
+++ app/_components/Layout/Header copy.jsx
@@ -0,0 +1,43 @@
+"use client"
+import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+const Header = () => {
+  const path = usePathname();
+  const navItems = [
+    {
+      display: "the camp.", slug: "/"
+    },
+    {
+      display: "the experience.", slug: "/experience"
+    },
+    {
+      display: "the blog.", slug: "/blog"
+    }
+  ]
+  
+
+  return (
+      // <header className={`header ${path === "/experience" || path === "/blog/takeoff" ? "header--light" : ""}`}>
+      // use the above line if you want to apply the light theme to the blog/takeoff page.
+      <header className={`header ${path === "/experience" ? "header--light" : ""}`}>
+      <img className="header__logo" src="/assets/logo.svg" width={200} height={200} alt="Logo" />
+      <ul className="header__nav">
+        {navItems.map( (item) => (
+       <li key={item.slug}>
+         <Link href={`${item.slug}`}>
+         <h5>{item.display}</h5>
+         </Link>
+       </li>
+
+        ))}
+      </ul>
+      <Link href="/events">
+      <button className="btn btn--black btn--small">Book Now</button>
+      </Link>
+  </header>
+  )
+}
+
+export default Header
